3e30111bc8533929394fc7916bd418dd8caecf6f
Aso <mafakheri.asoo@gmail.com>
2025-06-18 07:55:14 +0330

    Implement additional scoring functions

--- scoring_function.py
+++ scoring_function.py
@@ -1,4 +1,4 @@
-from math import log1p
+from math import log1p, sqrt
 
 def __get_common_neighbors(graph, v, u):
     neighbors_of_v = set(graph.neighbors(v))
@@ -23,3 +23,92 @@ def adamic_adar(graph, v, u):
     return sum(
         1/log1p(graph.degree(c_neighbor)) for c_neighbor in __get_common_neighbors(graph, v, u)
     )
+
+def resource_allocation(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    s = 0 
+    for n in common_neighbors:
+        s+= 1/graph.degree(n)
+    return s
+
+def salton(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    try:
+        s = len(common_neighbors) / sqrt(graph.degree(v) * graph.degree(u))
+    except ZeroDivisionError:
+        s = 0
+    return s
+
+def sorensen(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    try:
+        s = 2 * len(common_neighbors) / (graph.degree(v) + graph.degree(u))
+    except ZeroDivisionError:
+        s = 0
+    return s
+
+def hub_promoted_index(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    try:
+        s = len(common_neighbors) / min(graph.degree(v), graph.degree(u))
+    except ZeroDivisionError:
+        s = 0
+    return s
+
+def hub_depressed_index(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    try:
+        s = len(common_neighbors) / max(graph.degree(v), graph.degree(u))
+    except ZeroDivisionError:
+        s = 0
+    return s
+
+def weighted_preferential_attachment(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    s = 0
+    for z in common_neighbors:
+        s += (graph.get_edge_weight(v, z) * graph.get_edge_weight(z, u))
+    return s 
+  
+def weighted_common_neighbor(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    s = 0
+    for z in common_neighbors:
+        s += (graph.get_edge_weight(v, z) + graph.get_edge_weight(z, u))
+    return s 
+
+def weighted_adamic_adar(graph, v, u):
+  common_neighbors = __get_common_neighbors(graph, v, u)
+  s = 0
+  for z in common_neighbors:
+    s += (
+      (
+        graph.get_edge_weight(v, z) + graph.get_edge_weight(z, u)
+      ) / log1p(graph.node_strength(z))
+    )
+  return s
+
+def weighted_adamic_adar(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    s = 0
+    for z in common_neighbors:
+        s += (
+            (graph.get_edge_weight(v, z) + graph.get_edge_weight(z, u))
+            / log1p(graph.node_strength(z))
+        )
+    return s
+
+def weighted_jaccard(graph, v, u):
+    s1 = weighted_common_neighbor(graph, v, u)
+    s2 = graph.node_strength(v) + graph.node_strength(u)
+    return s1 / s2
+
+def weighted_resource_allocation(graph, v, u):
+    common_neighbors = __get_common_neighbors(graph, v, u)
+    s = 0
+    for z in common_neighbors:
+        s += (
+            (graph.get_edge_weight(v, z) + graph.get_edge_weight(z, u))
+            / graph.node_strength(z)
+        )
+    return s
